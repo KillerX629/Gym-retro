013091949294ba486a9fd80c87fc46220f28bc7d
Ignacio Pita <napita2007@gmail.com>
2022-04-22 13:21:12 -0300

    explicaciones de tamaño en main.py

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
 import retro
+#from agent import *
 
-env = retro.make(game="MortalKombat3-Genesis", state="Level1.ShangTsungVsLiuKang")
-obs = env.reset()
 
+
+
+
+"""Tamaño de la entrada:
+    224  320  3
+    height, width, channels
+    
+número de acciones posibles:
+    8
+    [izq,der]
+"""
+
+
+if __name__ == '__main__':
+    env = retro.make(game="MortalKombat3-Genesis",
+                    state="Level1.ShangTsungVsLiuKang")
+    obs = env.reset()
+    height, width, channels = env.observation_space.shape
+    print("", height, "", width, "", channels)
+    actions = env.action_space.n
+    
+    #dqn.fit(env, nb_steps=1000000, visualize=True, verbose=2)fit entrena el modelo
+
+    while True:
+        print("", height, "", width, "", channels)
+        action = env.action_space.sample()
+        obs, rew, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
+            env.close()
+
+
+    """
+    este código es una de las formas en las que podemos conseguir guardar
+    los píxeles de la pantalla en variables.
+    
+    alto, ancho, canales = obs.shape
+    
+    acciones = env.action_space.n
+    
+    
+    para conocer las acciones disponibles en el juego:
+    env.unwrapped.get_action_meanings()
+    """
+    
+    """
 while True:
     action = env.action_space.sample()
     obs, rew, done, info = env.step(action)
@@ -10,3 +56,4 @@ while True:
     if done:
         obs = env.reset()
         env.close()
+"""
